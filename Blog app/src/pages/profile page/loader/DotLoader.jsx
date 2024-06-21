2c4f1dfab9df45f8c69cc7b9ef5b2065aa900e54
Manish9026 <135260553+Manish9026@users.noreply.github.com>
2024-06-21 18:36:41 +0530

    update profile

---
+++ Blog app/src/pages/profile page/loader/DotLoader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.scss'
+const DotLoader = () => {
+  return (
+    <section className='dot-loader'>
+
+        <div className="loader"></div>
+    </section>
+  )
+}
+
+export default DotLoader
